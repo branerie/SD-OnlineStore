facc7f253aa9cd403af4eb37b90fc54ddbcc9161
b0yank <bnkaloyanov@gmail.com>
2020-11-12 15:56:51 +0200

    Fixed bug which didn't allow updates to product categories and sizes

--- online-store/src/components/adminCategories/index.js
+++ online-store/src/components/adminCategories/index.js
@@ -7,52 +7,58 @@ const AdminCategories = (props) => {
     const [categories, setCategories] = useState(getCategories())
     const [category, setCategory] = useState('')
     const id = props.id
-    
-    function getCategories() {
-       return props.categories.map(eachCategory => `${eachCategory}`).join(', ')
-   }
 
-   const addCategories = (event) => {
+    function getCategories() {
+        return props.categories.map(eachCategory => `${eachCategory}`).join(', ')
+    }
+
+    const addCategories = async (event) => {
         event.preventDefault()
 
-    fetch(`http://localhost:3001/api/admin/product/${id}/categories`, {
-        method: "PATCH",
-        body: JSON.stringify({
-            'operations': [{
-                'action': 'add',
-                'value': category
-            }]
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': getCookie('x-auth-cookie')
-        }
-    })
+        const response = await fetch(`http://localhost:3001/api/admin/product/${id}/categories`, {
+            method: "PATCH",
+            body: JSON.stringify({
+                'operations': [{
+                    'action': 'add',
+                    'value': category
+                }]
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': getCookie('x-auth-cookie')
+            }
+        })
+
+        const updatedCategories = await response.json()
+        setCategories(updatedCategories.categories.join(', '))
     }
 
     const removeCategories = async (event) => {
         event.preventDefault()
 
-    await fetch(`http://localhost:3001/api/admin/product/${id}/categories`, {
-        method: 'PATCH',
-        body: JSON.stringify({
-            'operations':{
-                'action': 'delete',
-                'value': category
+        const response = await fetch(`http://localhost:3001/api/admin/product/${id}/categories`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                'operations': [{
+                    'action': 'delete',
+                    'value': category
+                }]
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': getCookie('x-auth-cookie')
             }
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': getCookie('x-auth-cookie')
-        }
-    })
+        })
+
+        const updatedCategories = await response.json()
+        setCategories(updatedCategories.categories.join(', '))
     }
 
 
     return (
-       <form className={styles.container} onSubmit={addCategories}>
-           <div>Product is set in categories: <span>{categories}</span></div>
-           <Input 
+        <form className={styles.container} onSubmit={addCategories}>
+            <div>Product is set in categories: <span>{categories}</span></div>
+            <Input
                 type='text'
                 label='Edit categories field'
                 id='categories'
@@ -61,9 +67,9 @@ const AdminCategories = (props) => {
                 maxLength='20'
                 size='35'
             />
-           <button className={styles.button} type='submit'>Add categories</button>
-           <button className={styles.button} onClick={removeCategories}>Remove categories</button>
-       </form>
+            <button className={styles.button} type='submit'>Add categories</button>
+            <button className={styles.button} onClick={removeCategories}>Remove categories</button>
+        </form>
     )
 }
 
--- online-store/src/components/inputProductCard/index.js
+++ online-store/src/components/inputProductCard/index.js
@@ -20,7 +20,7 @@ const InputProductCard = (props) => {
     const [description, setDescription] = useState(props.description)
     const [gender, setGender] = useState(props.gender)
     const id = props.id
-   
+
     function getSizeAmounts() {
         return props.sizes.map(ps => `${ps.sizeName} - ${ps.count}`).join(', ')
     }
@@ -45,68 +45,68 @@ const InputProductCard = (props) => {
     }
 
     return (
-
-        <form className={styles.container} onSubmit={handleSubmit}>
-            <img alt='product' className={styles.img} src={image} />
-            <Input
-                type='text'
-                label='Brand'
-                id='brand'
-                value={brand}
-                onChange={e => setBrand(e.target.value)}
-                maxLength='30'
-            />
-            <Input
-                type='number'
-                label='Price'
-                id='price'
-                value={price}
-                onChange={e => setPrice(e.target.value)}
-                onBlur={() => {
-                    const formattedPrice = parseFloat(price).toFixed(2) 
-                    setPrice(formattedPrice)
-                }}
-            />
-            <Input
-                type='number'
-                label='Discount in %'
-                id='discount'
-                value={discountPercent}
-                onChange={e => setDiscountPercent(Number(e.target.value))}
-            />
-            <Input
-                type='date'
-                label='End date of discount'
-                id='discountEndDate'
-                value={discountEndDate}
-                onChange={e => setDiscountEndDate(e.target.value)}
-            />
-            <Input
-                type='text'
-                label='Product description'
-                id='description'
-                value={description}
-                onChange={e => setDescription(e.target.value)}
-            />
-            <div onChange={(e) => setGender(e.target.value)}>
-                <label>
-                    <input type="radio" value="M" name="gender" />
+        <div>
+            <form className={styles.container} onSubmit={handleSubmit}>
+                <img alt='product' className={styles.img} src={image} />
+                <Input
+                    type='text'
+                    label='Brand'
+                    id='brand'
+                    value={brand}
+                    onChange={e => setBrand(e.target.value)}
+                    maxLength='30'
+                />
+                <Input
+                    type='number'
+                    label='Price'
+                    id='price'
+                    value={price}
+                    onChange={e => setPrice(e.target.value)}
+                    onBlur={() => {
+                        const formattedPrice = parseFloat(price).toFixed(2)
+                        setPrice(formattedPrice)
+                    }}
+                />
+                <Input
+                    type='number'
+                    label='Discount in %'
+                    id='discount'
+                    value={discountPercent}
+                    onChange={e => setDiscountPercent(Number(e.target.value))}
+                />
+                <Input
+                    type='date'
+                    label='End date of discount'
+                    id='discountEndDate'
+                    value={discountEndDate}
+                    onChange={e => setDiscountEndDate(e.target.value)}
+                />
+                <Input
+                    type='text'
+                    label='Product description'
+                    id='description'
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                />
+                <div onChange={(e) => setGender(e.target.value)}>
+                    <label>
+                        <input type="radio" value="M" name="gender" />
                     Male
                 </label>
-                <label>
-                    <input type="radio" value="F" name="gender" />
+                    <label>
+                        <input type="radio" value="F" name="gender" />
                     Female
                 </label>
-                <label>
-                    <input type="radio" value="unspecified" name="gender" checked />
+                    <label>
+                        <input type="radio" value="unspecified" name="gender" checked />
                     Unspecified
                 </label>
-            </div>
-            <p>Amount : {sizes}</p>
-            <AdminCategories categories={props.categories} id={id}/>
-            <button type='submit'>SAVE</button>
-
-        </form>
+                </div>
+                <p>Amount : {sizes}</p>
+                <button type='submit'>SAVE</button>
+            </form>
+            <AdminCategories categories={props.categories} id={id} />
+        </div>
     )
 }
 
