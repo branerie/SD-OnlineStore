e35973ea11d56febd7bc0884492a563751e5aadc
toni-todorov <todorow.toni@gmail.com>
2020-11-25 15:45:27 +0200

    add hooks folder and setVisible hook

--- online-store/src/components/sortCriteria/index.js
+++ online-store/src/components/sortCriteria/index.js
@@ -1,11 +1,11 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useRef, useEffect} from 'react'
 import ProductsContext from '../../ProductsContext'
+import useVisible from '../../hooks/setVisible'
 import styles from './index.module.css'
 
 const SortCriteria = () => {
     const productsContext = useContext(ProductsContext)
-    const [open, setOpen] = useState(false)
-    const toggle = () => setOpen(!open)
+    const { ref, isVisible, setIsVisible } = useVisible(false)    
    
     const setSortCriteria = (criteria , direction) => {
 
@@ -17,10 +17,10 @@ const SortCriteria = () => {
 
     return (
         <div className={styles.dropDownMenu}>
-            <button className={styles.title} onClick={toggle}>Sort by:</button>
-            {open ? 
+            <button className={styles.title} onClick={e => setIsVisible(!isVisible)}>Sort by:</button>
+            {isVisible ? 
                 ( 
-                    <div className={styles.list}>                    
+                    <div className={styles.list} ref={ref}>                    
                         <a className={styles.criteria} onClick={() => setSortCriteria('date', 'desc')}>Newest Arrivals</a>      
                         <a className={styles.criteria} onClick={() => setSortCriteria('price', 'asc')}>Price: Low to High</a>
                         <a className={styles.criteria} onClick={() => setSortCriteria('price', 'desc')}>Price: High to Low</a>
