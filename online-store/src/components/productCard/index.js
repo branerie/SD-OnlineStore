1f17ea29b7c195facc9bcd91a80642dfd684ee87
toni.todorov <toni.todorov@smartdesign.local>
2020-12-02 12:13:13 +0200

    productCard was modified and not finish

--- online-store/src/components/productCard/index.js
+++ online-store/src/components/productCard/index.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import styles from './index.module.css'
 import cardFrame from '../../images/productCardFrame.svg'
+import favotiesImage from '../../images/favoritesLink.svg'
 
 const ProductCard = (props) => {
+    let discount = null   
+
+    if(props.discount) {
+        console.log(props.discount.percent)
+        const discPrice = props.discountPrice.toFixed(2)
+        const discPercent = props.discount.percent
+
+        discount = `-${parseInt(discPercent)}% = ${discPrice}$`
+    }
+   
 
     return (
         <div className={styles.container}>
-            <img src={cardFrame} className={styles['image-frame']}/>
+            <img src={cardFrame}/>
             <img src={props.images} alt={'No images'}  className={styles['product-image']}/>
-            <p className={styles.brand}>{props.brand}</p>
-            <p className={styles.price}>{props.price}</p>
-            <p className={styles.discount}>
-                { props.discountPrice ? props.discountPrice.toFixed(2) : null}</p>
+            <div className={styles['text-container']}>
+                <div className={styles['brand-likeButton']}>                    
+                    <div className={styles.brand}>{props.brand}</div>
+                    <img src={favotiesImage} className={styles['like-button']}/>                 
+                </div>
+                <div className={styles['price-container']}>
+                    <div className={styles.price}>{props.price}$</div>
+                    <div className={styles['discount-style']}>
+                        { props.discountPrice ? discount : null}</div>
+                </div>
+            </div>
         </div>
     )
 }
