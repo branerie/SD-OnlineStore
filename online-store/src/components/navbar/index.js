77bfbbdab2d0f4f88939971f80c673fc17b186fa
b0yank <bnkaloyanov@gmail.com>
2020-12-04 14:32:14 +0200

    Created initial site footer

--- online-store/src/components/navbar/index.js
+++ online-store/src/components/navbar/index.js
@@ -10,29 +10,43 @@ import profileImage from '../../images/profileLink.svg'
 
 import NavbarImageLink from '../navbarImageLink'
 
-const Navbar = () => {
+const Navbar = ({ clickedName }) => {
     const { user: { isAdmin } } = useContext(UserContext)
 
     return (
-        <nav className={styles['site-nav']}>
-            <ul className={styles['navbar-links']}>
-                <li className={styles['logo-space']}></li>
-                {/* <NavbarLink name='Men' path='/products?cat_gender=M'/> */}
-                <NavbarImageLink name='Men' path='/products?cat_gender=M' imageName='menLink.png' />
-                <NavbarImageLink name='Women' path='/products?cat_gender=F' imageName='menLink.png'  />
-                <NavbarImageLink name='Promotions' path='/products?bool_discount=true' imageName='menLink.png' />
-                {isAdmin
-                    ? <NavbarLink name='Admin' path='/admin/products' />
-                    : null
-                }
-            </ul>
+        <>
+            <nav className={styles['site-nav']}>
+                <div className={styles['expanded-bg']}></div>
+                <ul className={styles['navbar-links']}>
+                    {/* <NavbarLink name='Men' path='/products?cat_gender=M'/> */}
+                    <NavbarImageLink
+                        name='Women'
+                        path='/products?cat_gender=F'
+                        imageName='menLink.png'
+                        clicked={clickedName === 'Women'} />
+                    <NavbarImageLink
+                        name='Men'
+                        path='/products?cat_gender=M'
+                        imageName='menLink.png'
+                        clicked={clickedName === 'Men'} />
+                    <NavbarImageLink
+                        name='Promotions'
+                        path='/products?bool_discount=true'
+                        imageName='menLink.png'
+                        clicked={clickedName === 'Promotions'} />
+                    {isAdmin
+                        ? <NavbarLink name='Admin' path='/admin/products' />
+                        : null
+                    }
+                </ul>
+            </nav>
             <ul className={styles['navbar-buttons']}>
                 <NavbarSearch />
                 <NavbarImageButton path='' imageSrc={favoritesImage} />
                 <NavbarImageButton path='' imageSrc={cartImage} />
                 <NavbarImageButton path='' imageSrc={profileImage} />
             </ul>
-        </nav>
+        </>
     )
 }
 
