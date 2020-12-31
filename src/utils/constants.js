2ac2e860543fe387d0a3520a54a516063b08428b
b0yank <bnkaloyanov@gmail.com>
2020-12-31 16:09:10 +0200

    Fixed REST API URL for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,9 @@
 const AUTH_COOKIE_NAME = 'x-auth-cookie'
 const REST_API_URL = process.env.NODE_ENV === 'production'
-                            ? 'https://sd-onlinestore.herokuapp.com'
+                            ? 'https://sd-onlinestore.herokuapp.com/api'
                             : 'http://localhost:3001/api'
 const JSON_CONTENT_TYPE = 'application/json'
+
 
 const FACEBOOK_APP_ID = '1270748659962291'
 const GOOGLE_CLIENT_ID = '765190029748-2kkc40ku90opksnnsa97givv1ka0td2j.apps.googleusercontent.com'
