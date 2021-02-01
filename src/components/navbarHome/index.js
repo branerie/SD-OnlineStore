adc5ed66f46eb512ee752e89817cb4f42c3c36f1
toni.todorov <toni.todorov@smartdesign.local>
2021-02-01 18:53:35 +0200

    modified hamburger menu

--- src/components/navbarHome/index.js
+++ src/components/navbarHome/index.js
@@ -1,23 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styles from './index.module.css'
 import UserContext from '../../UserContext'
 import NavbarLink from '../navbarLink'
 import NavButtons from '../navButtons'
-import { useVisible } from '../../hooks'
 
 const NavbarHome = () => {
     const { user: { isAdmin }} = useContext(UserContext)
-    const { ref, isVisible, setIsVisible } = useVisible(false)
+    const [isActive, setIsActive] = useState(false)
 
     return (
         <nav className={styles['site-nav']}>
             <ul className={styles['navbar-links']}>
-                <div className={ isVisible ? `${styles['menu-toggle']} ${styles['is-active']}` : styles['menu-toggle'] } onClick={() => setIsVisible(!isVisible)}>
+                <div className={ isActive ? `${styles['menu-toggle']} ${styles['is-active']}` : styles['menu-toggle'] } onClick={() => setIsActive(!isActive)}>
                     <span className={styles.bar}></span>
                     <span className={styles.bar}></span>
                     <span className={styles.bar}></span>
                 </div>
-                <div className={ isVisible ? `${styles['nav-menu']} ${styles.active}` : styles['nav-menu']} ref={ref}>
+                <div className={ isActive ? `${styles['nav-menu']} ${styles.active}` : styles['nav-menu']}>
                     <NavbarLink name='Men' path='/products/men'/>
                     <NavbarLink name='Women' path='/products/women'/>
                     <NavbarLink name='Promotions' path='/products/promotions'/>
--- src/components/productsFilter/index.js
+++ src/components/productsFilter/index.js
@@ -1,15 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 import styles from './index.module.css'
 import ProductsCheckboxFilter from '../productsCheckboxFilter'
 import ProductsRangeFilter from '../productsRangeFilter'
 import ProductContext from '../../ProductsContext'
 import ProductsRatingFilters from '../productsRatingFilters'
-import { useVisible } from '../../hooks'
 
 const ProductsFilter = () => {
     const { productProps, productPage, totalCount } = useContext(ProductContext)
-    const { ref, isVisible, setIsVisible } = useVisible(false)
+    const [isActive, setIsActive] = useState(false)
 
     
     if (!productProps || !productPage) {
@@ -23,12 +22,12 @@ const ProductsFilter = () => {
     return (
         <>
             <div className={styles.container}>
-                <div className={ isVisible ? `${styles['menu-toggle']} ${styles['is-active']}` : styles['menu-toggle'] } onClick={() => setIsVisible(!isVisible)}>
+                <div className={ isActive ? `${styles['menu-toggle']} ${styles['is-active']}` : styles['menu-toggle'] } onClick={() => setIsActive(!isActive)}>
                         <span className={styles.bar}></span>
                         <span className={styles.bar}></span>
                         <span className={styles.bar}></span>
                 </div>
-                <div className={ isVisible ? ` ${styles['media-aside']} ${styles.active}` : styles['media-aside']} ref={ref}>
+                <div className={ isActive ? ` ${styles['media-aside']} ${styles.active}` : styles['media-aside']}>
                     <div className={styles.title}>
                         <p className={styles['center-text']}>
                             Showing {pageLength} of {totalCount} results
