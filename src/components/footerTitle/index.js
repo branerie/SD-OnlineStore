f4e1da38d2ccf85f4978924f6bac2141a8e273be
b0yank <bnkaloyanov@gmail.com>
2021-03-18 12:41:36 +0200

    Deleted all components to fix renaming

--- src/components/footerTitle/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './index.module.css'
-
-const FooterTitle = ({ title }) => {
-    return <h3 className={styles['column-title']}>{title}</h3>
-}
-
-export default FooterTitle
--- src/components/pageSecondaryTitle/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './index.module.css'
-
-const PageSecondaryTitle = ({ title, style }) => {
-    return <div className={styles.title} style={style}>{title}</div>
-}
-
-export default PageSecondaryTitle
